80876cc0090c2da62c41b32f1a7118ee6a8ae661
Tejobhasa <tejobhasa.mm@gmail.com>
2024-04-25 14:37:05 +0630

    first commit

---
+++ script.js
@@ -0,0 +1,7 @@
+// TODO: Write Your JavaScript Code Here
+var students = ["Zaw Mg", "Zin Min", "Zaw Htet", "Su Myat", "Su Lat"];
+console.log(students.length);
+
+for (var i = 0; i < students.length; i = i + 1){
+    console.log("Great to see you, " + students[i] + "!");
+};
